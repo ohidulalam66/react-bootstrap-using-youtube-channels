52f98fa5ddd00e27eff4a374a684c329e58a2c89
ohidulalam <mdnoyonctg15@gmail.com>
2021-09-29 14:08:49 +0600

    footer style added

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 #### Everyone can learn programming language from these youtube channels.Here I have given you some information about these channels.These are the world's top programming youtube channel.
 
+#### It's a top and popular programming YouTube channel's website, I think for those who want to learn programming can learn a lot as programming from these notable people channel. I respect them.
+
 [Top Programming Related Youtube Channels](https://optimistic-wescoff-686bad.netlify.app/ "Top Programming Related Youtube Channels")
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,19 +4,19 @@ import './Footer.css';
 const Footer = () => {
     return (
         <>
-            <div className= "text-white footer-container">
+            <div className= "text-white footer-bg footer-container">
                 <div className= "about-section">
                 <div>
                     <h2>About ME</h2>
-                    <p>My name is Mohammad Ohidul Alam. I am a student of programming heroes. I am studying in CSE department. It's a top and popular programming YouTube channel's website, I think for those who want to learn programming can learn a lot as programming from these notable people channel. I respect them.</p>
+                    <p>My name is <b className= "text-danger fs-5">Mohammad Ohidul Alam</b>. I am a student of programming heroes. I am studying in CSE department. It's a top and popular programming YouTube channel's website, I think for those who want to learn programming can learn a lot as programming from these notable people channel. I respect them.</p>
                 </div>
                 <ul>
                     <a href="/face"><i class="fab fa-facebook"></i></a>
                     <a href="https://github.com/ohidulalam66" target= "_blank."><i class="fab fa-github"></i></a>
-                    <a href="/instagram"><i class="fas fa-at"></i></a>
+                    <a href="https://mdnoyonctg15@gmail.com"><i class="fas fa-at"></i></a>
                 </ul>
                 </div>
-                <img src="./profile.jpg"alt="" />
+                <img src="./profile-pic.png"alt="" />
             </div>
         </>
     );
